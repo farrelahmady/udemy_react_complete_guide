651029096cfa608918083e690cf8e44146f99c52
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-08-28 01:54:32 +0700

    bottom-up component communication

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
-const NewExpense = () => {
-  return (
-    <div className='new-expense'>
-      <ExpenseForm />
-    </div>
-  );
+const NewExpense = (props) => {
+	const onSaveExpenseDataHandler = (expenseData) => {
+		const expense = {
+			...expenseData,
+			id: Math.floor(Math.random() * 1000000),
+		};
+
+		props.onAddExpense(expense);
+	};
+
+	return (
+		<div className="new-expense">
+			<ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+		</div>
+	);
 };
 
 export default NewExpense;
