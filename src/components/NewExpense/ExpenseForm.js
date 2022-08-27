8a94b3b4e1f027e28fba9b3bb370fca00db32747
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-08-28 01:34:43 +0700

    two way binding input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -9,7 +9,7 @@ const ExpenseForm = () => {
 
 	const titleChangeHandler = (event) => {
 		setEnteredTitle(event.target.value);
-		console.log(enteredTitle);
+		//console.log(enteredTitle);
 	};
 
 	const amountChangeHandler = (event) => {
@@ -30,6 +30,9 @@ const ExpenseForm = () => {
 		};
 
 		console.log(expenseData);
+		setEnteredTitle("");
+		setEnteredAmount("");
+		setEnteredDate("");
 	};
 
 	return (
@@ -37,7 +40,11 @@ const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input
+						type="text"
+						onChange={titleChangeHandler}
+						value={enteredTitle}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
@@ -46,6 +53,7 @@ const ExpenseForm = () => {
 						min="0.01"
 						step="0.01"
 						onChange={amountChangeHandler}
+						value={enteredAmount}
 					/>
 				</div>
 				<div className="new-expense__control">
@@ -55,6 +63,7 @@ const ExpenseForm = () => {
 						min="2019-01-01"
 						max="2022-12-31"
 						onChange={dateChangeHandler}
+						value={enteredDate}
 					/>
 				</div>
 			</div>
